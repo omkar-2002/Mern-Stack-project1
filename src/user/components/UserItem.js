95388c1265adc691bf3d60b69c42ea12b2261e19
omkar-2002 <omkarsalapurkar2002@gmail.com>
2022-01-25 23:07:04 +0530

    starting template

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const UserItem = props =>{
+
+}
+
+
+export default UserItem;
---
+++ src/user/components/UserList.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const UserList = props =>{
+
+}
+
+
+export default UserList;
